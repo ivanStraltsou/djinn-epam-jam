03927afb08e1583e2cfaff93a2517da28f7acb97
Ivan Sharamet <isharamet@XPBYMINW0016.minsk.epam.com>
2018-11-24 13:18:12 +0300

    Add implementation for pubsub-to-elasticsearch funciton

---
+++ gcloud/functions/pubsub-to-elasticsearch/index.js
@@ -0,0 +1,26 @@
+/**
+ * Background Cloud Function to be triggered by Pub/Sub.
+ * This function is exported by index.js, and executed when
+ * the trigger topic receives a message.
+ *
+ * @param {object} data The event payload.
+ * @param {object} context The event metadata.
+ */
+exports.publishToElasticsearch = async (event, context) => {
+    const pubsubMessage = event.data;
+    if (pubsubMessage.data) {
+        var message = Buffer.from(pubsubMessage.data, 'base64').toString();
+
+        console.info("[TELEMETRY]", message);
+
+        var client = new elasticsearch.Client({
+            hosts: ["35.205.16.214:9200"]
+        });
+
+        var a = await client.index({
+            index: 'telemetry',
+            type: "_doc",
+            body: JSON.parse(message)
+        });
+    }
+};
